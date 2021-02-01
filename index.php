133be2f80499fbc75898cfb64c60dba6c480c20a
Anatoly Kostarev <0711975@gmail.com>
2021-02-02 01:02:16 +0300

    Task_2 - initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task_2: Условия</title>
+</head>
+<body>
+    <?php
+
+
+
+
+
+    ?>
+</body>
+</html>
